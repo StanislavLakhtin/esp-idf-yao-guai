bb8cdc288b23edbf400595e41c981e45c595858a
Stanislav Lakhtin <sl@whiteants.net>
2020-03-22 16:35:34 +0300

    minor changes

--- main/screens/settings.c
+++ main/screens/settings.c
@@ -2,28 +2,34 @@
 // Created by sl on 21.03.2020.
 //
 
+#include "esp_log.h"
 #include "lvgl.h"
 #include "screens.h"
 
-static void group_focus_cb(lv_group_t * group);
+static const char * TAG = "settings";
 
-static lv_obj_t * setting_win;
-static lv_group_t * g;
+static void group_focus_cb(lv_group_t * group);
+static void wifi_scan_event_cb(lv_obj_t * obj, lv_event_t event);
+
+    static lv_obj_t * setting_win = NULL;
+static lv_group_t * g = NULL;
 
 void construct_settings_screen(void) {
+  if ( setting_win != NULL)
+    return; // is already constructed
   setting_win = lv_win_create(lv_scr_act(), NULL);
   lv_win_set_title(setting_win, "Settings");
   g = lv_group_create();
   lv_group_set_focus_cb(g, group_focus_cb);
   /* Add EXIT control button to the header */
-  lv_obj_t * close_btn = lv_win_add_btn(setting_win, LV_SYMBOL_CLOSE);
-  lv_obj_set_event_cb(close_btn, event_cb);
-  lv_group_add_obj(g, close_btn);
+  lv_obj_t * _btn = lv_win_add_btn(setting_win, LV_SYMBOL_CLOSE);
+  lv_obj_set_event_cb(_btn, event_cb);
+  lv_group_add_obj(g, _btn);
   lv_indev_set_group(encoder_indev, g);
   lv_obj_t * obj;
-  obj = lv_spinbox_create(setting_win, NULL);
-  //lv_obj_set_event_cb(obj, event_cb);
-  lv_spinbox_set_digit_format(obj, 5, 2);
+  obj = lv_list_create(setting_win, NULL);
+  _btn = lv_list_add_btn(obj, LV_SYMBOL_WIFI, "Scan WiFi");
+  lv_obj_set_event_cb(_btn, );
   lv_group_add_obj(g, obj);
 }
 
@@ -31,6 +37,11 @@ static void group_focus_cb(lv_group_t * group)
 {
   lv_obj_t * f = lv_group_get_focused(group);
   if(f != setting_win) lv_win_focus(setting_win, f, LV_ANIM_ON);
+}
+
+static void wifi_scan_event_cb(lv_obj_t * obj, lv_event_t event) {
+  ESP_LOGI(TAG, "wifi_scan_event_cb");
+  event_cb(obj, event);
 }
 
 void event_cb(lv_obj_t * obj, lv_event_t event) {
