762de74e5448e20bafc7a5150a5d3e722b1c072a
Stanislav Lakhtin <sl@whiteants.net>
2020-03-22 23:37:38 +0300

    minor changes

--- main/encoder/rotary_encoder.c
+++ main/encoder/rotary_encoder.c
@@ -62,11 +62,12 @@ encoder_t encoder0;
 void encoders_conf() {
   event_queue = xQueueCreate(5, sizeof(btns_event_t));
 
-  gpio_config_t gpio_conf;
-  gpio_conf.intr_type = GPIO_INTR_NEGEDGE;
-  gpio_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-  gpio_conf.mode = GPIO_MODE_INPUT;
-  gpio_conf.pull_up_en = 1;
+  gpio_config_t gpio_conf = {
+      .intr_type = GPIO_INTR_NEGEDGE,
+      .pin_bit_mask = GPIO_INPUT_PIN_SEL,
+      .mode = GPIO_MODE_INPUT,
+      .pull_up_en = 1
+  };
   gpio_config(&gpio_conf);
   gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
 
