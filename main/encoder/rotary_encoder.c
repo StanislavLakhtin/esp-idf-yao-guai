1c4c474a298b058812d4b7e5d15e9f4880b43714
Stanislav Lakhtin <sl@whiteants.net>
2020-03-29 14:34:12 +0300

    btn/enc conf

--- main/encoder/rotary_encoder.c
+++ main/encoder/rotary_encoder.c
@@ -48,33 +48,29 @@ void encoders_conf() {
 }
 
 void button_init(button_t * btn, gpio_num_t pin, btns_event_t on_press, btns_event_t on_release) {
-  gpio_config_t gpio_conf;
-  gpio_conf.pin_bit_mask = pin;
-  gpio_conf.mode = GPIO_MODE_INPUT;
-  gpio_conf.pull_up_en = 1;
-  gpio_conf.intr_type = GPIO_INTR_ANYEDGE;
-  gpio_config(&gpio_conf);
-
   btn->pin = pin;
   btn->state = BTN_IDLE;
   btn->on_press_event = on_press;
   btn->on_release_event = on_release;
 
+  gpio_set_direction(btn->pin, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(btn->pin, GPIO_PULLUP_ONLY);
+  gpio_set_intr_type(btn->pin, GPIO_INTR_ANYEDGE);
   gpio_isr_handler_add(pin, buttons_isr_handler, btn);
 }
 
 void encoder_init(encoder_t * encoder, gpio_num_t dir_pin, gpio_num_t int_pin, btns_event_t on_left, btns_event_t on_right) {
-  gpio_config_t gpio_conf;
-  gpio_conf.pin_bit_mask = (1ULL<<dir_pin) | (1ULL<<int_pin);
-  gpio_conf.mode = GPIO_MODE_INPUT;
-  gpio_conf.intr_type = GPIO_INTR_POSEDGE;
-  gpio_conf.pull_up_en = 1;
-  gpio_config(&gpio_conf);
-
   encoder->dir_pin = dir_pin;
   encoder->int_pin = int_pin;
   encoder->on_left = on_left;
   encoder->on_right = on_right;
+
+  gpio_set_direction(encoder->dir_pin, GPIO_MODE_INPUT);
+  gpio_set_direction(encoder->int_pin, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(encoder->dir_pin, GPIO_PULLUP_ONLY);
+  gpio_set_pull_mode(encoder->int_pin, GPIO_PULLUP_ONLY);
+
+  gpio_set_intr_type(int_pin, GPIO_INTR_POSEDGE);
 
   gpio_isr_handler_add(encoder->int_pin, encoder_isr_handler, encoder);
 }
